5aeb1886cb87cbbe6e2743c614f97459f1bea15a
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2023-10-14 15:51:00 +0200

    modified:   ../blue is clues/s1/Blue's.Clues.S01E01.en.srt 	new file:   resource/blues.clues.intro.en.srt 	renamed:    example/the.magic.school.bus.1994.song.en.srt -> resource/the.magic.school.bus.1994.song.en.srt 	modified:   working.srt.py

--- tool/working.srt.py
+++ tool/working.srt.py
@@ -12,9 +12,11 @@ srt_subs = []
 parser = argparse.ArgumentParser(description='Working SRT')
 parser.add_argument('srt_path', nargs='+')
 parser.add_argument('-d', '--delay', type=float)
+parser.add_argument('-r', '--replace', action='store_true')
 args = parser.parse_args()
 
-delay = args.delay
+delay      = args.delay
+is_replace = args.replace
 
 for i in range(len(args.srt_path)):
     srt_path.append(args.srt_path[i])
@@ -32,9 +34,16 @@ for i in range(len(srt_path)):
        srt_subs[i] = list(srt.parse(f))
 
 text = []
-for subs in srt_subs:
-    for _, sub in enumerate(subs):
+
+if is_replace and len(srt_path) == 2:
+    for i, sub in enumerate(srt_subs[1]):
+        if len(srt_subs[0]) > i:
+            sub = srt_subs[0][i]
         text.append(sub)
+else:
+    for subs in srt_subs:
+        for _, sub in enumerate(subs):
+            text.append(sub)
 
 for i, sub in enumerate(text):
     sub.index = i+1
